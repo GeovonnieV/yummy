75fd6f98cea316e4859e1e5f804723f5fe38e397
unknown <geovonnnievasquez@gmail.com>
2021-05-03 12:29:43 -0500

    calorie filter

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,12 @@ import ReactCard from "./components/RecipeCard";
 import "./styles/app.scss";
 import RecipeCard from "./components/RecipeCard";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { InputGroup, Button, FormControl } from "react-bootstrap";
+import {
+  InputGroup,
+  Button,
+  FormControl,
+  Form
+} from "react-bootstrap";
 
 function App() {
   // API Keys
@@ -13,6 +18,7 @@ function App() {
   const [recipes, setRecipies] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("rice");
+  const [searchCalories, setSearchCalories] = useState("");
 
   //  runs when query is changed
   useEffect(() => {
@@ -21,7 +27,7 @@ function App() {
   // api call
   const getRecipies = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=6`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=6&calories=0-${searchCalories}`
     );
     const data = await response.json();
     // sets recipe state to results
@@ -33,19 +39,27 @@ function App() {
     setSearch(e.target.value);
     console.log(search);
   };
-  // set the query to whats in search
+  // makes the query state the search value
   const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
     setSearch("");
+  };
+  //
+  const getSearchCalories = (e) => {
+    e.preventDefault()
+   console.log("hello")
   };
 
   // JSX
   return (
     <div className="container">
       <h1 className="home-head display-1">Yummy</h1>
-      <p style={{"color": "#fff"}}>5 star chef or 1st time meal maker? No matter the case Yummy will turn everyday ingredients to Yummy plates!</p>
-      {/* form */}
+      <p style={{ color: "#fff" }}>
+        5 star chef or 1st time meal maker? No matter the case Yummy will turn
+        everyday ingredients to Yummy plates!
+      </p>
+      {/* Search form */}
       <form onSubmit={getSearch} className="search-form">
         <InputGroup className="mb-3">
           <FormControl
@@ -57,24 +71,29 @@ function App() {
             onChange={updateSearch}
           />
           <InputGroup.Append>
-            <Button variant="outline-secondary" type="submit">Search</Button>
+           {/*  */}
+           <button value={"hello"} onClick={getSearchCalories}>hello</button>
+            {/*  */}
+            <Button variant="outline-secondary" type="submit">
+              Search
+            </Button>
           </InputGroup.Append>
         </InputGroup>
       </form>
       {/* form end */}
-        <p style={{"color": "#fff"}}>(*use commas to seprerate ingredients)</p>
+      <p style={{ color: "#fff" }}>(*use commas to seprerate ingredients)</p>
       <div className="recipe-div">
-      {recipes.map((recipe) => (
-        <RecipeCard
-        title={recipe.recipe.label}
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-        carbs={Math.round(recipe.recipe.digest[1].total)}
-        fats={Math.round(recipe.recipe.digest[0].total)}
-        protein={Math.round(recipe.recipe.digest[2].total)}
-        />
-      ))}
+        {recipes.map((recipe) => (
+          <RecipeCard
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            carbs={Math.round(recipe.recipe.digest[1].total)}
+            fats={Math.round(recipe.recipe.digest[0].total)}
+            protein={Math.round(recipe.recipe.digest[2].total)}
+          />
+        ))}
       </div>
     </div>
   );
